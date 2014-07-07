065083d437ace2e7f0ec4b3a947ebb4daf0dddc7
Andrey Kutalev <akutalev@gmail.com>
2014-07-07 20:28:55 +0700

    MyBatis java config has been added. Config properties files for mysql, oracle, postgresql have been added.

---
+++ web-prototype/src/main/java/it/sevenbits/project/application/configs/datasource/MyBatisConfig.java
@@ -0,0 +1,95 @@
+package it.sevenbits.project.application.configs.datasource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+
+/**
+ * MyBatis Configuration (for database)
+ */
+@Configuration
+@PropertySource("file:${project.config}/configurations/application/jdbc-mysql-template.properties")
+
+/*****************************************************************/
+/* @MapperScan - myBatis annotation. Shows path to data mappers  */
+/* if you want use another ORM then you should erase this string */
+/* and switch another ORM (See documentation for it)             */
+/*****************************************************************/
+@MapperScan("it.sevenbits.project.application.repositories")
+
+@EnableTransactionManagement
+public class MyBatisConfig {
+
+    @Inject
+    private Environment environment;
+
+    /**
+     * DataSource Bean setup
+     * @return DataSource
+     */
+    @Bean
+    public DataSource dataSource() {
+
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        ds.setUrl(environment.getProperty("jdbc.url"));
+        ds.setUsername(environment.getProperty("jdbc.username"));
+        ds.setPassword(environment.getProperty("jdbc.password"));
+        ds.setMaxActive(Integer.valueOf(environment.getProperty("jdbc.max-active")));
+        ds.setMaxWait(Integer.valueOf(environment.getProperty("jdbc.max-wait")));
+        ds.setInitialSize(Integer.valueOf(environment.getProperty("jdbc.connections-number")));
+        ds.setValidationQuery(environment.getProperty("jdbc.validation-query"));
+        ds.setPoolPreparedStatements(true);
+        ds.setDefaultAutoCommit(true);
+        ds.setTestOnBorrow(true);
+        ds.setRemoveAbandonedTimeout(10);
+        ds.setRemoveAbandoned(true);
+
+        return ds;
+    }
+
+    /**
+     * SQL Session Factory setup
+     * @return SQL Session Factory
+     * @throws Exception if fails
+     */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+
+        /************************************/
+        /* MyBatis configuration            */
+        /* Set default value for NULL type  */
+        /* Used for Oracle DB               */
+        /************************************/
+        Resource resource = new ClassPathResource("mybatis-config.xml");
+        sqlSessionFactory.setConfigLocation(resource);
+
+        return sqlSessionFactory.getObject();
+    }
+
+    /**
+     * Transaction Manager Setup
+     * @return transaction manager
+     */
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+
+        return new DataSourceTransactionManager(dataSource());
+    }
+}
